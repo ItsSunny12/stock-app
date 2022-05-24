e39d2b6d596ddc4ecaa100d2402cbc83c35afc30
Manuel Sepulveda <ms.lopez94@hotmail.com>
2022-05-24 15:14:47 -0400

    cambiamos el texto de nodemon

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) =>{
     console.log("peticion recibida");
 
-    res.send("<h1>hola mundo con nodemon</h1>");
+    res.send("<h1>hola mundo</h1>");
 });
 
 app.listen(4000, () => {
