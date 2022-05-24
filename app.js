dfd30156bae928673ea7b4b5230ef32eaef567fa
Manuel Sepulveda <ms.lopez94@hotmail.com>
2022-05-24 17:23:43 -0400

    cambios

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 app.get('/', (req, res) =>{
     console.log("peticion recibida");
 
-    res.send('<h1>hola mundo</h1>');
+    res.send('<h1>Camila Te Amo , Mañosita</h1>');
 });
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
