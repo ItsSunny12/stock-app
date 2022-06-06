54f41cc2774021433d9e21901f34bb9cb336c176
Manuel Sepulveda <ms.lopez94@hotmail.com>
2022-06-06 14:06:57 -0400

    voexion MDB + html + css y JS

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    console.log("peticion recibida");
+mongoose
+    .connect(
+        `mongodb+srv://msLopez94:${process.env.MONGO_DB_PASS}@development.qvtb9oq.mongodb.net/stock-app?retryWrites=true&w=majority`
+    )
+    .then((result) => { 
+        app.listen(PORT, () => {
+            console.log(`Servidor escuchando en el puerto ${PORT}`);
+        });
+        console.log('conexion exitosa BBDD')
+        
+    })   
+    .catch((err) => console.log(err));
 
-    res.send('<h1>Camila Te Amo , Mañosita</h1>');
+const productSchema = mongoose.Schema(
+ {
+    name: { type: String, require: true },
+    price: Number,
+ },
+ {timestamps: true}
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+app.use(express.json());  
+
+
+
+app.post('/api/v1/products', (req, res, next) =>{
+     
+    const newProduct = new Product(req.body)
+
+    newProduct.save()
+    .then((result) => {
+        res.status(201).json({ok: true});
+
+    })
+    .catch((err) => console.log(err))
+
 });
 
-const PORT = process.env.PORT
+app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
